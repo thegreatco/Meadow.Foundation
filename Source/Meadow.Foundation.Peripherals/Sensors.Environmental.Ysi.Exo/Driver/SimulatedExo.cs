c7046efa0a396b69eb7eef51689ec76e9c523440
Chris Tacke <ctacke@opennetcf.com>
2025-05-20 10:36:14 -0500

    adding simulated Exo sonde

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Environmental.Ysi.Exo/Driver/SimulatedExo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Meadow.Foundation.Sensors.Environmental.Ysi;
+
+/// <summary>
+/// A class providing simulated EXO sonde behavior
+/// </summary>
+public class SimulatedExo : IExoSonde
+{
+    /// <inheritdoc/>
+    public Task ForceSample()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public Task<(ParameterCode ParameterCode, object Value)[]> GetCurrentData()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public Task<Exo.ParameterStatus[]> GetParameterStatus()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public Task<ParameterCode[]> GetParametersToRead()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public Task<TimeSpan> GetSamplePeriod()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public Task SetParametersToRead(ParameterCode[] parameters)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public Task SetSamplePeriod(TimeSpan period)
+    {
+        throw new NotImplementedException();
+    }
+}
