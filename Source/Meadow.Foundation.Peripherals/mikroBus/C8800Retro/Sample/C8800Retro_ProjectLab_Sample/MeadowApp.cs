0f449fec8a7e8f968c389ff017aacee0f78bd3fe
Chris Tacke <ctacke@opennetcf.com>
2025-06-11 21:22:52 -0500

    moving mikrobus libs into foundation

---
+++ Source/Meadow.Foundation.Peripherals/mikroBus/C8800Retro/Sample/C8800Retro_ProjectLab_Sample/MeadowApp.cs
@@ -0,0 +1,60 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Graphics;
+using Meadow.Foundation.mikroBUS.Displays;
+using System;
+using System.Threading.Tasks;
+
+namespace C8800Retro_ProjectLab_Sample
+{
+    // Change F7FeatherV2 to F7FeatherV1 for V1.x boards
+    public class MeadowApp : App<F7CoreComputeV2>
+    {
+        //<!=SNIP=>
+
+        C8800Retro altair;
+        IProjectLabHardware projectLab;
+
+        MicroGraphics graphics;
+
+        public override Task Initialize()
+        {
+            Console.WriteLine("Initializing ...");
+
+            projectLab = ProjectLab.Create();
+
+            altair = new C8800Retro(projectLab.MikroBus1.I2cBus, projectLab.MikroBus1.Pins.INT);
+
+            var button1B = altair.GetButton(C8800Retro.ButtonColumn._1, C8800Retro.ButtonRow.B);
+            button1B.Clicked += Button1B_Clicked;
+
+            graphics = new MicroGraphics(altair)
+            {
+                CurrentFont = new Font4x8(),
+            };
+
+            return base.Initialize();
+        }
+
+        private void Button1B_Clicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("Button 1B clicked");
+        }
+
+        public override async Task Run()
+        {
+            altair.EnableBlink(true, true);
+
+            graphics.Clear();
+            graphics.DrawText(0, 0, "MF", Color.White);
+            graphics.Show();
+
+            await Task.Delay(6000);
+
+            altair.EnableBlink(false);
+        }
+
+        //<!=SNOP=>
+    }
+}
---
+++ Source/Meadow.Foundation.Peripherals/mikroBus/CMBusMaster/Sample/CMBusMaster_ProjectLab_Sample/MeadowApp.cs
@@ -0,0 +1,51 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.MBus.RelayMBus;
+using Meadow.Foundation.mikroBUS.Sensors.MBus;
+using System;
+using System.Threading.Tasks;
+
+namespace CMBusMaster_ProjectLab_Sample
+{
+    // Change F7FeatherV2 to F7FeatherV1 for V1.x boards
+    public class MeadowApp : App<F7CoreComputeV2>
+    {
+        //<!=SNIP=>
+
+        private CMBusMaster master;
+        private IProjectLabHardware projectLab;
+        private PadPulsM2 pulseCounter;
+
+        public override Task Initialize()
+        {
+            Resolver.Log.Info("Initializing ...");
+
+            projectLab = ProjectLab.Create();
+
+            master = new CMBusMaster(
+                projectLab.MikroBus1.CreateSerialPort(
+                    baudRate: 9600,
+                    parity: Meadow.Hardware.Parity.Even)
+                );
+
+            pulseCounter = new PadPulsM2(master);
+
+            return Task.CompletedTask;
+        }
+
+        public override async Task Run()
+        {
+            pulseCounter.StartMonitoring();
+
+            Resolver.Log.Info($"Ports: {pulseCounter.Ports[0].ID:X8} {pulseCounter.Ports[1].ID:X8}");
+
+            while (true)
+            {
+                Resolver.Log.Info($"Counts: {pulseCounter.Ports[0].CurrentCount:X8} {pulseCounter.Ports[1].CurrentCount:X8}");
+                await Task.Delay(TimeSpan.FromSeconds(5));
+            }
+        }
+
+        //<!=SNOP=>
+    }
+}
