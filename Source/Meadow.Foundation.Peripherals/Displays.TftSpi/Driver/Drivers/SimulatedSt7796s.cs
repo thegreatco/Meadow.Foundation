964a3b8e470b6cecd9c8804c3b50656963a92148
Adrian Stevens <adrian.stevens@gmail.com>
2025-08-21 22:58:12 -0700

    Add more simulated display drivers

---
+++ Source/Meadow.Foundation.Peripherals/Displays.TftSpi/Driver/Drivers/SimulatedSt7796s.cs
@@ -0,0 +1,26 @@
+﻿using Meadow.Peripherals.Displays;
+
+namespace Meadow.Foundation.Displays;
+
+/// <summary>
+/// Represents a simulated St7796s displayRenderer
+/// </summary>
+public class SimulatedSt7796s : SimulatedDisplayBase
+{
+    /// <summary>
+    /// The color modes supported by the display
+    /// </summary>
+    public override ColorMode SupportedColorModes => ColorMode.Format16bppRgb565 | ColorMode.Format12bppRgb444;
+
+    /// <summary>
+    /// Create a new simulated StSt7796s displayRenderer.
+    /// </summary>
+    /// <param name="displayRenderer"></param>
+    /// <param name="rotate"></param>rotate
+    /// <param name="colorMode"></param>
+    public SimulatedSt7796s(IResizablePixelDisplay displayRenderer,
+        bool rotate = true,
+        ColorMode colorMode = ColorMode.Format12bppRgb444)
+        : base(displayRenderer, 320, 480, rotate, colorMode)
+    { }
+}
